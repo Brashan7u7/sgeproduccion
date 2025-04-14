04b50ba3bdd82e5fe23960990521e44ba4d785bf
Brashan7u7 <84888260+Brashan7u7@users.noreply.github.com>
2025-04-14 02:44:05 -0600

    Cambio de direccion

--- frontend/dist/browser/chunk-7MRC6WDX.js
+++ frontend/dist/browser/chunk-7MRC6WDX.js
@@ -1,1 +1,36 @@
-import{M as p,R as l,Yb as s,_b as c}from"./chunk-R3GSGODC.js";var r="http://localhost:3000/expedientes",b=class a{constructor(t){this._http=t}getExpedienteById(t){return this._http.get(`${r}/${t}`)}getExpedientes(t=1,e=10,n,o){let i=`${r}?page=${t}&limit=${e}`;return n&&(i+=`&vigencia_documental=${n}`),o&&(i+=`&num_seg_social=${o}`),this._http.get(i)}getExpedientesnum_seg_social(t){return this._http.get(`http://localhost:3000/search/num_seg_socialocial/${t}`)}createExpediente(t){let e=new s({"Content-Type":"application/json"});return this._http.post(`${r}`,t,{headers:e})}updateExpediente(t,e){let n=new s({"Content-Type":"application/json"});return this._http.patch(`${r}/${t}`,e,{headers:n})}static \u0275fac=function(e){return new(e||a)(l(c))};static \u0275prov=p({token:a,factory:a.\u0275fac,providedIn:"root"})};export{b as a};
+import { M as p, R as l, Yb as s, _b as c } from "./chunk-R3GSGODC.js";
+var r = "http://192.168.1.76:3000/expedientes",
+  b = class a {
+    constructor(t) {
+      this._http = t;
+    }
+    getExpedienteById(t) {
+      return this._http.get(`${r}/${t}`);
+    }
+    getExpedientes(t = 1, e = 10, n, o) {
+      let i = `${r}?page=${t}&limit=${e}`;
+      return (
+        n && (i += `&vigencia_documental=${n}`),
+        o && (i += `&num_seg_social=${o}`),
+        this._http.get(i)
+      );
+    }
+    getExpedientesnum_seg_social(t) {
+      return this._http.get(
+        `http://192.168.1.76:3000/search/num_seg_socialocial/${t}`
+      );
+    }
+    createExpediente(t) {
+      let e = new s({ "Content-Type": "application/json" });
+      return this._http.post(`${r}`, t, { headers: e });
+    }
+    updateExpediente(t, e) {
+      let n = new s({ "Content-Type": "application/json" });
+      return this._http.patch(`${r}/${t}`, e, { headers: n });
+    }
+    static ɵfac = function (e) {
+      return new (e || a)(l(c));
+    };
+    static ɵprov = p({ token: a, factory: a.ɵfac, providedIn: "root" });
+  };
+export { b as a };
--- frontend/dist/browser/chunk-ISZDIOUG.js
+++ frontend/dist/browser/chunk-ISZDIOUG.js
@@ -1,1 +1,24 @@
-import{M as o,R as i,_b as n}from"./chunk-R3GSGODC.js";var e="http://localhost:3000/usuarios",s=class r{constructor(t){this._http=t}getUsuarios(){return this._http.get(e)}createUsuario(t){return this._http.post(e,t)}updateUsuario(t,a){return this._http.patch(`${e}/${t}`,a)}deleteUsuario(t){return this._http.delete(`${e}/${t}`)}static \u0275fac=function(a){return new(a||r)(i(n))};static \u0275prov=o({token:r,factory:r.\u0275fac,providedIn:"root"})};export{s as a};
+import { M as o, R as i, _b as n } from "./chunk-R3GSGODC.js";
+var e = "http://192.168.1.76:3000/usuarios",
+  s = class r {
+    constructor(t) {
+      this._http = t;
+    }
+    getUsuarios() {
+      return this._http.get(e);
+    }
+    createUsuario(t) {
+      return this._http.post(e, t);
+    }
+    updateUsuario(t, a) {
+      return this._http.patch(`${e}/${t}`, a);
+    }
+    deleteUsuario(t) {
+      return this._http.delete(`${e}/${t}`);
+    }
+    static ɵfac = function (a) {
+      return new (a || r)(i(n));
+    };
+    static ɵprov = o({ token: r, factory: r.ɵfac, providedIn: "root" });
+  };
+export { s as a };
